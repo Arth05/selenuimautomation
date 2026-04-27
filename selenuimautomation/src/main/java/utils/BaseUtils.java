0df6de718b90b9b8913bdf5f3c02205ec5074172
Arth <137848147+Arth05@users.noreply.github.com>
2026-04-27 18:52:58 -0400

    Updated the code

--- selenuimautomation/src/main/java/utils/BaseUtils.java
+++ selenuimautomation/src/main/java/utils/BaseUtils.java
@@ -1,7 +1,14 @@
 package utils;
 
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 public class BaseUtils {
 	
@@ -21,6 +28,13 @@ public class BaseUtils {
 	
 	public static String getProperty(String key) {
         return properties.getProperty(key);
+    }
+	
+	public static String getScreenShotPath(WebDriver driver, String pageName) throws IOException {
+        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir") + "/target/screenshots/image_" + pageName + ".png";
+        FileUtils.copyFile(source, new File(path));
+        return path;
     }
 
 }
--- selenuimautomation/src/test/java/selenuimautomation/BaseTest.java
+++ selenuimautomation/src/test/java/selenuimautomation/BaseTest.java
@@ -1,27 +1,61 @@
 package selenuimautomation;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import managers.DriverManager;
 import managers.ExtentManager;
 import managers.ExtentTestManager;
+import utils.BaseUtils;
 
 public class BaseTest {
-	
+
 	@BeforeSuite
-	public void initialise () {
+	public void initialise() {
 		ExtentManager.initReport();
 	}
-	
+
 	@BeforeMethod
 	public void preReq(Method method) {
 		ExtentTestManager.createTest(method.getName());
 	}
 
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.SUCCESS) {
+
+			ExtentTestManager.log.pass("Test Passed");
+
+		} else if (result.getStatus() == ITestResult.FAILURE) {
+
+			String screenshotName = result.getInstance().getClass().getSimpleName() + "."
+					+ result.getMethod().getMethodName();
+
+			String screenshotPath = BaseUtils.getScreenShotPath(DriverManager.getDriver(), screenshotName);
+
+			ExtentTestManager.log.fail(result.getThrowable(),
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+
+		}
+
+		else if (result.getStatus() == ITestResult.SKIP) {
+
+			ExtentTestManager.log.skip("Test Skipped");
+		}
+
+	}
+
 	@AfterSuite
-	public void tearDown () {
+	public void tearDown() {
 		ExtentManager.flushReport();
 	}
 }
